9103a580f5f79da59cde18e34cacb4c08d86beec
sewonjun <thornapple74@gmail.com>
2023-11-16 20:41:47 +0900

    chore: firebase serviceAccount 전부 env 처리

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -3,21 +3,7 @@ const { getAuth } = require("firebase-admin/auth");
 const admin = require("firebase-admin");
 const User = require("../models/User");
 
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PROJECT_ID,
-  // private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-sg32l%40team-dbx.iam.gserviceaccount.com",
-  universe_domain: "googleapis.com",
-};
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
